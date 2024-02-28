f8fc7e6957ad8512803bdb766686233c76925fef
NandaKishoreJana <110282188+NandaKishoreJana@users.noreply.github.com>
2024-02-28 12:05:33 +0530

    version1

---
+++ addnumbers.java
@@ -0,0 +1,10 @@
+
+public class addnumbers {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 10;
+        int sum = number1 + number2;
+        System.out.println("Sum of " + number1 + " and " + number2 + " is " + sum);
+    }
+}
+
